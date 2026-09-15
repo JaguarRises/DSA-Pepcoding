8167899f470441cb12e864e322379d5b7caf5f99
Prerit Rathi <diplomatprerit@gmail.com>
2026-09-15 22:52:32 +0530

    Indentations

--- Basics/Conditionals.java
+++ Basics/Conditionals.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
+
 public class Conditionals {
     public static void main(String[] args) {
-        
-        int x = 9;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the value of x: ");
+        int x = scanner.nextInt();
 
         if(x % 2 == 0){
-
             System.out.println(x + " is Even");
-        }else{
+        }
+        else
+        {
             System.out.println(x + " is Odd");
-
         }
 
         System.out.println("Hardwork is better than smart work");
